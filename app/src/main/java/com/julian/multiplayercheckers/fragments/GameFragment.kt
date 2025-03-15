6363bb07be2c12ea632e5255581f478885e06469
Julian Bednarek <juliuszxxbednarek@gmail.com>
2025-03-15 20:03:46 +0100

    .

--- app/src/main/java/com/julian/multiplayercheckers/fragments/GameFragment.kt
+++ app/src/main/java/com/julian/multiplayercheckers/fragments/GameFragment.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,22 +20,24 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.julian.multiplayercheckers.R
 import com.julian.multiplayercheckers.composables.GameField
 import com.julian.multiplayercheckers.enums.FieldStates
 import com.julian.multiplayercheckers.viewmodels.GameViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.coroutines.flow.StateFlow
 
 const val BOARD_SIZE: Int = 8
 val NOT_USED_COLOR: Color = Color.White
 val USED_COLOR: Color = Color.Black
 
 @AndroidEntryPoint
-class GameFragment @Inject constructor(
-    private val viewModel: GameViewModel
-) : Fragment() {
+class GameFragment : Fragment() {
+
+    private val viewModel: GameViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,7 +46,7 @@ class GameFragment @Inject constructor(
         return ComposeView(requireContext()).apply {
             setContent {
                 requireArguments().getInt("USER_TURN").let { viewModel.userTurn = it }
-                requireArguments().getString("GAME_TOKEN")?.let { GameFragmentLayout(it) }
+                requireArguments().getString("GAME_TOKEN")?.let { GameFragmentLayout(it, viewModel) }
             }
         }
     }
@@ -64,7 +68,7 @@ fun GameFragmentLayout(gameToken: String, viewModel: GameViewModel = hiltViewMod
             for (i in 0 until BOARD_SIZE) {
                 Row {
                     for (j in 0 until BOARD_SIZE) {
-                        ProcessBoardField(viewModel.board[i][j])
+                        ProcessBoardField(viewModel.board, i, j)
                     }
                 }
             }
@@ -73,13 +77,16 @@ fun GameFragmentLayout(gameToken: String, viewModel: GameViewModel = hiltViewMod
 }
 
 @Composable
-fun ProcessBoardField(field: Int) {
-    when(field) {
-        FieldStates.NOT_USED.value  -> GameField(NOT_USED_COLOR, FieldStates.NOT_USED)
-        FieldStates.EMPTY.value     -> GameField(USED_COLOR, FieldStates.EMPTY)
-        FieldStates.PLAYER_1.value  -> GameField(USED_COLOR, FieldStates.PLAYER_1)
-        FieldStates.PLAYER_2.value  -> GameField(USED_COLOR, FieldStates.PLAYER_2)
-        FieldStates.PLAYER_1_QUEEN.value -> GameField(USED_COLOR, FieldStates.PLAYER_1_QUEEN)
-        FieldStates.PLAYER_2_QUEEN.value -> GameField(USED_COLOR, FieldStates.PLAYER_2_QUEEN)
+fun ProcessBoardField(board: StateFlow<Array<Array<FieldStates>>>, row: Int, col: Int) {
+    val boardState by board.collectAsState()
+    when(boardState[row][col]) {
+        FieldStates.NOT_USED  -> GameField(NOT_USED_COLOR, FieldStates.NOT_USED)
+        FieldStates.EMPTY     -> GameField(USED_COLOR, FieldStates.EMPTY)
+        FieldStates.PLAYER_1  -> GameField(USED_COLOR, FieldStates.PLAYER_1)
+        FieldStates.PLAYER_2  -> GameField(USED_COLOR, FieldStates.PLAYER_2)
+        FieldStates.PLAYER_1_QUEEN -> GameField(USED_COLOR, FieldStates.PLAYER_1_QUEEN)
+        FieldStates.PLAYER_2_QUEEN -> GameField(USED_COLOR, FieldStates.PLAYER_2_QUEEN)
+        // TODO CHANGE IT TO PROPER HINT
+        FieldStates.HINT -> GameField(NOT_USED_COLOR, FieldStates.NOT_USED)
     }
 }
--- app/src/main/java/com/julian/multiplayercheckers/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/julian/multiplayercheckers/viewmodels/GameViewModel.kt
@@ -9,9 +9,13 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.julian.multiplayercheckers.dataclasses.GameData
+import com.julian.multiplayercheckers.enums.FieldStates
 import com.julian.multiplayercheckers.fragments.BOARD_SIZE
 import com.julian.multiplayercheckers.viewmodels.GameHostingViewModel.Companion.TOKENS
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,7 +25,12 @@ class GameViewModel @Inject constructor(
 ) : AndroidViewModel(application) {
     private val _canMove: MutableLiveData<Boolean> = MutableLiveData(false)
     val canMove: LiveData<Boolean> get() = _canMove
-    val board: Array<IntArray> = Array(BOARD_SIZE) { IntArray(BOARD_SIZE) }
+    private val _board = MutableStateFlow(Array(BOARD_SIZE) { _ ->
+        Array(BOARD_SIZE) { _ ->
+            FieldStates.EMPTY
+        }
+    })
+    val board: StateFlow<Array<Array<FieldStates>>> = _board.asStateFlow()
 
     var userTurn: Int = 0
         set(value) {
@@ -35,6 +44,7 @@ class GameViewModel @Inject constructor(
                 if (snapshot.exists()) {
                     val gameData: GameData = snapshot.getValue(GameData::class.java)!!
                     _canMove.value = gameData.whosTurn == userTurn
+                    updateBoard(gameData.gameState)
                 }
             }
 
@@ -43,5 +53,33 @@ class GameViewModel @Inject constructor(
             }
         }
         gameStateReference.addValueEventListener(gameStateListener)
+    }
+
+    private fun updateBoard(gameState: String) {
+        val newBoard = Array(BOARD_SIZE) { row ->
+            Array(BOARD_SIZE) { col -> board.value[row][col] }
+        }
+        var index = 0
+        for (row in 0 until BOARD_SIZE) {
+            for (col in 0 until BOARD_SIZE) {
+                if ((row + col) % 2 == 1) {
+                    if (index < gameState.length) {
+                        val stateField: Char = gameState[index]
+                        newBoard[row][col] = when(stateField) {
+                            'r' -> FieldStates.PLAYER_1
+                            'R' -> FieldStates.PLAYER_1_QUEEN
+                            'n' -> FieldStates.EMPTY
+                            'b' -> FieldStates.PLAYER_2
+                            'B' -> FieldStates.PLAYER_2_QUEEN
+                            else -> FieldStates.EMPTY
+                        }
+                        index++
+                    }
+                } else {
+                    newBoard[row][col] = FieldStates.NOT_USED
+                }
+            }
+        }
+        _board.value = newBoard
     }
 }
